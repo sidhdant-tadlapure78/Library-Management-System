be0f8790fcbac1bdf34bd2cdd28ab0c103a035b6
Sidhdant <sidhdanttadlapure1@gmail.com>
2026-06-01 22:11:34 +0530

    new changes in java file

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,7 @@ class Hello
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
-}
--- src/test/java/Demo.java.bak
+++ src/test/java/Demo.java.bak
@@ -3,6 +3,5 @@ class Demo
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		String name="sidhdant";
 	}
 }
